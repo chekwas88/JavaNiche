f294c868533af8b58ecb670e0db404754c751e90
Chisom Onwuchekwa <princechekwas@gmail.com>
2019-08-26 11:30:03 +0100

    config jest and setup circleci

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  coverageDirectory: 'coverage',
+  collectCoverage: true,
+  setupFiles: ['<rootDir>/enzyme.config.js'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!(jest-)?react-native||react-navigation)',
+    '<rootDir>/assets/img'
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg)$':
+      '<rootDir>/mocks/fileMock.js',
+  },
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    "!**/coverage/**",
+    '!**/node_modules/**',
+    '!jest.config.js',
+    "!**/babel.config.js",
+    '!**/coverage/**',
+    '!**/_tests_/**',
+  ],
+};
